67125d623bc67552789b305511e621b5680e07b5
Alexis Hinkle <84148777+Amhinkle@users.noreply.github.com>
2022-04-27 17:34:48 +0100

    Add files via upload

---
+++ Project-Kelvin/script.js
@@ -0,0 +1,9 @@
+// Todays Forecast
+const kelvin = 293;
+// Converting Kelvin to Celsius
+var celsius = 293 - 272;
+// Calculation Fahrenheit
+var fahrenheit = celsius * (9 / 5) + 32;
+// Founding down fahrenheit temp
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
