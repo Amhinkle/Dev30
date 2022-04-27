3f3206ced62f98aee8e21e642b4538ba871f9e54
Alexis Hinkle <84148777+Amhinkle@users.noreply.github.com>
2022-04-27 17:51:19 +0100

    Update script.js

--- Project-Dog-Years/script.js
+++ Project-Dog-Years/script.js
@@ -1,14 +1,15 @@
-// Declaring my age
+// This is my Age
 var myAge = 26;
-// Declaring early years 
 var earlyYears = 2;
 earlyYears *= 10.5;
-// Declaring later years
+
+// Accounting for eary years
 laterYears = myAge -= 2;
-// Multiplying by 4 and assigning the new value
+// after the first 2 years, multiply by 4 instead of 10
 laterYears *= 4;
-// This result will give my age in dog years
+// combin early and later years to get dog years 
 myAgeInDogYears = earlyYears + laterYears;
 // putting my name to all lower case
 var myName = 'Allie'.toLowerCase();
+
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
